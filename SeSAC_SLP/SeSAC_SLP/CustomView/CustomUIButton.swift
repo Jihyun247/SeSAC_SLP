a828dea9bed06a2b4b2ff429796d0df005296581
Jihyun247 <wlguslove12345@gmail.com>
2022-01-19 23:56:53 +0900

    SLP-3 button extension

---
+++ SeSAC_SLP/SeSAC_SLP/CustomView/CustomUIButton.swift
@@ -0,0 +1,115 @@
+//
+//  CustomUIButton.swift
+//  SeSAC_SLP
+//
+//  Created by 김지현 on 2022/01/19.
+//
+
+import Foundation
+import UIKit
+
+
+class InactiveButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        self.isEnabled = false
+        self.setBorderColorAndRadius(borderColor: .sesacGray4, borderWidth: 0.5, cornerRadius: 8)
+        self.setTitleColor(.sesacBlack, for: .normal)
+        self.setBackgroundColor(.white, for: .normal)
+        self.titleLabel?.font = .body3_R14
+    }
+}
+
+class FillButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        self.isEnabled = true
+        self.setBorderColorAndRadius(borderColor: .sesacGreen, borderWidth: 0.5, cornerRadius: 8)
+        self.setTitleColor(.white, for: .normal)
+        self.setBackgroundColor(.sesacGreen!, for: .normal)
+        self.titleLabel?.font = .body3_R14
+    }
+}
+
+class OutlineButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        self.isEnabled = true
+        self.setBorderColorAndRadius(borderColor: .sesacGreen, borderWidth: 0.5, cornerRadius: 8)
+        self.setTitleColor(.sesacGreen, for: .normal)
+        self.setBackgroundColor(.white, for: .normal)
+        self.titleLabel?.font = .body3_R14
+    }
+}
+
+class CancelButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        self.isEnabled = true
+        self.setBorderColorAndRadius(borderColor: .sesacGray2, borderWidth: 0.5, cornerRadius: 8)
+        self.setTitleColor(.sesacBlack, for: .normal)
+        self.setBackgroundColor(.sesacGray2!, for: .normal)
+        self.titleLabel?.font = .body3_R14
+    }
+}
+
+class DisableButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        self.isEnabled = false
+        self.setBorderColorAndRadius(borderColor: .sesacGray6, borderWidth: 0.5, cornerRadius: 8)
+        self.setTitleColor(.sesacGray3, for: .normal)
+        self.setBackgroundColor(.sesacGray6!, for: .normal)
+        self.titleLabel?.font = .body3_R14
+    }
+}
---
+++ SeSAC_SLP/SeSAC_SLP/Extension/UIButton+Extension.swift
@@ -0,0 +1,70 @@
+//
+//  UIButton+Extension.swift
+//  SeSAC_SLP
+//
+//  Created by 김지현 on 2022/01/19.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    
+    func inactive(text: String) {
+        self.isEnabled = false
+        self.setBorderColorAndRadius(borderColor: .sesacGray4, borderWidth: 0.5, cornerRadius: 8)
+        self.setTitle(text, for: .normal)
+        self.setTitleColor(.sesacBlack, for: .normal)
+        self.setBackgroundColor(.white, for: .normal)
+        self.titleLabel?.font = .body3_R14
+    }
+    
+    func fill(text: String) {
+        self.isEnabled = true
+        self.setBorderColorAndRadius(borderColor: .sesacGreen, borderWidth: 0.5, cornerRadius: 8)
+        self.setTitle(text, for: .normal)
+        self.setTitleColor(.white, for: .normal)
+        self.setBackgroundColor(.sesacGreen!, for: .normal)
+        self.titleLabel?.font = .body3_R14
+    }
+    
+    func outline(text: String) {
+        self.isEnabled = true
+        self.setBorderColorAndRadius(borderColor: .sesacGreen, borderWidth: 0.5, cornerRadius: 8)
+        self.setTitle(text, for: .normal)
+        self.setTitleColor(.sesacGreen, for: .normal)
+        self.setBackgroundColor(.white, for: .normal)
+        self.titleLabel?.font = .body3_R14
+    }
+    
+    func cancel(text: String) {
+        self.isEnabled = true
+        self.setBorderColorAndRadius(borderColor: .sesacGray2, borderWidth: 0.5, cornerRadius: 8)
+        self.setTitle(text, for: .normal)
+        self.setTitleColor(.sesacBlack, for: .normal)
+        self.setBackgroundColor(.sesacGray2!, for: .normal)
+        self.titleLabel?.font = .body3_R14
+    }
+    
+    func disable(text: String) {
+        self.isEnabled = false
+        self.setBorderColorAndRadius(borderColor: .sesacGray7, borderWidth: 0.5, cornerRadius: 8)
+        self.setTitle(text, for: .normal)
+        self.setTitleColor(.sesacGray3, for: .normal)
+        self.setBackgroundColor(.sesacGray7!, for: .normal)
+        self.titleLabel?.font = .body3_R14
+    }
+    
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        UIGraphicsBeginImageContext(CGSize(width: 1.0, height: 1.0))
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.setFillColor(color.cgColor)
+        context.fill(CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0))
+        
+        let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+         
+        self.setBackgroundImage(backgroundImage, for: state)
+    }
+    
+}
