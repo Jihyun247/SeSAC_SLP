2938b84dad9d83e6328a5bb1297bcdb868d52528
Jihyun247 <wlguslove12345@gmail.com>
2022-02-17 02:22:53 +0900

    [ADD] searchbar & collectionview 연결

--- SeSAC_SLP/SeSAC_SLP/Main/MVVM/ViewController/StartSearchViewController.swift
+++ SeSAC_SLP/SeSAC_SLP/Main/MVVM/ViewController/StartSearchViewController.swift
@@ -8,10 +8,20 @@
 import Foundation
 import UIKit
 import RxSwift
+import RxGesture
+import Toast
 
 class StartSearchViewController: UIViewController {
     
     // MARK: - 인스턴스
+    var style: ToastStyle = {
+        var style = ToastStyle()
+        style.backgroundColor = .sesacGray3
+        style.messageColor = .sesacBlack
+        style.messageFont = .body4_R12!
+        return style
+    }()
+    
     let mainView = StartSearchView()
     let viewModel = StartSearchViewModel()
     let httpViewModel = MapHTTPViewModel()
@@ -38,7 +48,14 @@ class StartSearchViewController: UIViewController {
     
     func binding() {
         
-        let input = StartSearchViewModel.Input(aroundQueue: httpViewModel.exploreResult , hobbyText: mainView.searchBar.rx.text)
+        self.view.rx.tapGesture()
+            .when(.recognized)
+            .subscribe { _ in
+                self.view.endEditing(true)
+            }
+            .disposed(by: disposeBag)
+        
+        let input = StartSearchViewModel.Input(aroundQueue: httpViewModel.exploreResult, myHobbyText: mainView.searchBar.rx.text, tap: mainView.searchBar.searchTextField.rx.controlEvent(.editingDidEndOnExit))
         let output = viewModel.transform(input: input)
         
         output.othersHobby.bind(to: mainView.otherHobbyCollectionView.rx.items(cellIdentifier: HobbyCollectionViewCell.identifier, cellType: HobbyCollectionViewCell.self)) { (row, element, cell) in
@@ -46,15 +63,44 @@ class StartSearchViewController: UIViewController {
             cell.setup(hobbyType: .otherHobby)
             row < self.viewModel.recommendHobby.value.count ? cell.hobbyButton.recommended() : cell.hobbyButton.near()
             cell.hobbyButton.setTitle(element, for: .normal)
+            
+            cell.hobbyButton.rx.tap
+                .subscribe { _ in
+                   print("other hobby tapped")
+                }
+                .disposed(by: self.disposeBag)
         }
         .disposed(by: disposeBag)
         
-        output.recommendHobby.bind(to: mainView.myHobbyCollectionView.rx.items(cellIdentifier: HobbyCollectionViewCell.identifier, cellType: HobbyCollectionViewCell.self)) { (row, element, cell) in
+        viewModel.myHobby.bind(to: mainView.myHobbyCollectionView.rx.items(cellIdentifier: HobbyCollectionViewCell.identifier, cellType: HobbyCollectionViewCell.self)) { (row, element, cell) in
 
             cell.setup(hobbyType: .myHobby)
             cell.hobbyButton.setTitle(element, for: .normal)
+            
+            cell.hobbyButton.rx.tap
+                .subscribe { _ in
+                   print("my hobby tapped")
+                }
+                .disposed(by: self.disposeBag)
         }
         .disposed(by: disposeBag)
+        
+        output.myHobbyLengthValid
+            .subscribe { status in
+                status == false ? self.view.makeToast("최소 한 자 이상, 최대 8글자까지 작성 가능합니다", duration: 1.0, position: .center, style: self.style) : nil
+            }
+            .disposed(by: disposeBag)
+
+        
+        output.returnTapped
+            .subscribe { _ in
+                if self.viewModel.valid.value {
+                    self.viewModel.setMyHobbyArray(string: self.mainView.searchBar.text ?? "")
+                } else {
+                    self.view.makeToast("최소 한 자 이상, 최대 8글자까지 작성 가능합니다", duration: 1.0, position: .center, style: self.style)
+                }
+            }
+            .disposed(by: disposeBag)
     }
     
     func setCollectionView() {
@@ -102,14 +148,17 @@ extension StartSearchViewController: UICollectionViewDelegateFlowLayout {
             if collectionView == mainView.otherHobbyCollectionView {
                 label.text = viewModel.othersHobby.value[indexPath.item]
             } else {
-                label.text = "example!!"
+                label.text = viewModel.myHobby.value[indexPath.item]
             }
-            //tagList[indexPath.item]
             label.sizeToFit()
             return label
         }()
         
         let size = label.frame.size
-        return CGSize(width: size.width + 16, height: size.height + 16)
+        if collectionView == mainView.otherHobbyCollectionView {
+            return CGSize(width: size.width + 16, height: size.height + 16)
+        } else {
+            return CGSize(width: size.width + 32, height: size.height + 16)
+        }
     }
 }
--- SeSAC_SLP/SeSAC_SLP/Main/MVVM/ViewModel/StartSearchViewModel.swift
+++ SeSAC_SLP/SeSAC_SLP/Main/MVVM/ViewModel/StartSearchViewModel.swift
@@ -19,8 +19,14 @@ class StartSearchViewModel {
     let nearHobby = BehaviorRelay<[String]>(value: [])
     let othersHobby = BehaviorRelay<[String]>(value: [])
     
+    var myHobbyArray: [String] = []
+    let myHobby = BehaviorRelay<[String]>(value: [])
+    
+    var valid = BehaviorRelay<Bool>(value: false)
+    
     struct Input {
         let aroundQueue: PublishSubject<AroundQueue>
+        let myHobbyText: ControlProperty<String?>
         let tap: ControlEvent<Void>
     }
 
@@ -29,10 +35,11 @@ class StartSearchViewModel {
         let nearHobby: Observable<[String]>
         let othersHobby: Observable<[String]>
         //let myHobby: Observable<[String]>
+        let myHobbyLengthValid: Observable<Bool>
+        let returnTapped: ControlEvent<Void>
     }
 
     func transform(input: Input) -> Output {
-
 
         let recommendHobby =  input.aroundQueue.map { $0.fromRecommend }
         recommendHobby.bind(to: self.recommendHobby).disposed(by: disposeBag)
@@ -43,7 +50,37 @@ class StartSearchViewModel {
         
         let othersHobby = Observable.combineLatest(recommendHobby, nearHobby) { $0 + $1 }
         othersHobby.bind(to: self.othersHobby).disposed(by: disposeBag)
+        
+        let myHobby = input.myHobbyText
+            .orEmpty
+            .distinctUntilChanged()
+            .map { hobbyString -> [String] in
+                print(hobbyString)
+                let hobbyArray = hobbyString.components(separatedBy: " ")
+                return hobbyArray
+            }
+            .share(replay: 1, scope: .whileConnected)
+        
+        let valid = myHobby
+            .map { myHobbyArray -> Bool in
 
-        return Output(recommendHobby: recommendHobby, nearHobby: nearHobby, othersHobby: othersHobby)
+                let lastHobby = myHobbyArray.last ?? ""
+                let condition: Bool = lastHobby.count <= 8 && lastHobby.count > 0
+                
+                return condition
+            }
+            .share(replay: 1, scope: .whileConnected)
+        valid.bind(to: self.valid).disposed(by: disposeBag)
+
+
+        return Output(recommendHobby: recommendHobby, nearHobby: nearHobby, othersHobby: othersHobby, myHobbyLengthValid: valid, returnTapped: input.tap)
+    }
+    
+    func setMyHobbyArray(string: String) {
+        
+        myHobbyArray.append(contentsOf: string.components(separatedBy: " ").filter {
+            !myHobbyArray.contains($0)
+        })
+        myHobby.accept(self.myHobbyArray)
     }
 }
